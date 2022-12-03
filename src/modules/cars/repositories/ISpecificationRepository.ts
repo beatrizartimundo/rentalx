ecf2db2eb4206bd7df6c99af77b459303a5d759e
beatrizartimundo <beatriz.artimundo@gmail.com>
2022-12-03 09:18:47 -0300

    feat:create retalx apis

---
+++ src/modules/cars/repositories/ISpecificationRepository.ts
@@ -0,0 +1,14 @@
+import { Specification } from '../model/Specification';
+
+interface ICreateSpecificationDTO {
+  name: string;
+  description: string;
+}
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+interface IspecificationRepository {
+  create({ name, description }: ICreateSpecificationDTO): void;
+  findByName(name: string): Specification;
+}
+
+export { IspecificationRepository, ICreateSpecificationDTO };
---
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -0,0 +1,24 @@
+import { IspecificationRepository } from '../../repositories/ISpecificationRepository';
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+class CreateSpecificationUseCase {
+  constructor(private specificationsRepository: IspecificationRepository) {}
+  execute({ name, description }: IRequest): void {
+    const specificationAlreadyExists =
+      this.specificationsRepository.findByName(name);
+
+    if (specificationAlreadyExists) {
+      throw new Error('Specification Already Exists!');
+    }
+    this.specificationsRepository.create({
+      name,
+      description,
+    });
+  }
+}
+
+export { CreateSpecificationUseCase };
