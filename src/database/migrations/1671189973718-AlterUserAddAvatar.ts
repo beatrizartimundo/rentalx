e6d1238d6b1fab2090b333069356a08563218b15
beatrizartimundo <beatriz.artimundo@gmail.com>
2022-12-16 17:40:32 -0300

    test delete avatar

--- src/database/migrations/1671189973718-AlterUserAddAvatar.ts
+++ src/database/migrations/1671189973718-AlterUserAddAvatar.ts
@@ -1,6 +1,6 @@
 import {MigrationInterface, QueryRunner, TableColumn} from "typeorm";
 
-export class AlterUserAddAvatar1671138583374 implements MigrationInterface {
+export class AlterUserAddAvatar1671189973718 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.addColumn(
@@ -9,12 +9,11 @@ export class AlterUserAddAvatar1671138583374 implements MigrationInterface {
             name: "avatar",
             type: "varchar",
             isNullable: true,
-        })
-    );
-}
+        })  )
+  }
 
-public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropColumn("users", "avatar")
-}
+  public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropColumn("users", "avatar")
+  }
 
 }
