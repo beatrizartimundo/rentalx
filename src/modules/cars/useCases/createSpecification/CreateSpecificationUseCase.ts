ba0e513d4e38aba94fe2da5a1d2665615e0608be
beatrizartimundo <beatriz.artimundo@gmail.com>
2022-12-20 09:42:56 -0300

    add paths imports

--- src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -1,6 +1,6 @@
 import { inject, injectable } from 'tsyringe';
-import { AppError } from '../../../../errors/AppError';
-import { IspecificationRepository } from '../../repositories/ISpecificationRepository';
+import { AppError } from '@errors/AppError';
+import { IspecificationRepository } from '@modules/cars/repositories/ISpecificationRepository';
 
 interface IRequest {
   name: string;
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,10 +1,10 @@
 import { container } from "tsyringe";
-import { ICategoriesRepository } from "../../modules/cars/repositories/ICategoriesRepository";
-import { CategoriesRepository } from "../../modules/cars/repositories/implementations/CategoriesRepository";
-import { SpecificationRepository } from "../../modules/cars/repositories/implementations/SpecificationRepository";
-import { IspecificationRepository } from "../../modules/cars/repositories/ISpecificationRepository";
-import { IUsersRepository } from "../../modules/accounts/repositories/IUsersRepository";
-import { UsersRepository } from "../../modules/accounts/repositories/implementations/UsersRepository";
+import { ICategoriesRepository } from "@modules/cars/repositories/ICategoriesRepository";
+import { CategoriesRepository } from "@modules/cars/repositories/implementations/CategoriesRepository";
+import { SpecificationRepository } from "@modules/cars/repositories/implementations/SpecificationRepository";
+import { IspecificationRepository } from "@modules/cars/repositories/ISpecificationRepository";
+import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { UsersRepository } from "@modules/accounts/repositories/implementations/UsersRepository";
 
 
 container.registerSingleton<ICategoriesRepository>("CategoriesRepository",CategoriesRepository);
